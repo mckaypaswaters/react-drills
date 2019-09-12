24ae89a86f3c5904ed5d3a3d91c94a3b4711fd39
McKay Waters <mckaypaswaters@gmail.com>
2019-09-12 16:32:07 -0600

    finished drill 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Image from './Image'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      imgUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9fb0e132-4e14-4b4c-8fc3-7c6fe7db635a/dcg8r17-21125f93-f63e-4235-adbd-af2ae78af624.jpg/v1/fill/w_1024,h_576,q_75,strp/super_smash_bros_ultimate_lucina_wallpaper_by_leadingdemon0_dcg8r17-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTc2IiwicGF0aCI6IlwvZlwvOWZiMGUxMzItNGUxNC00YjRjLThmYzMtN2M2ZmU3ZGI2MzVhXC9kY2c4cjE3LTIxMTI1ZjkzLWY2M2UtNDIzNS1hZGJkLWFmMmFlNzhhZjYyNC5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.Qi0WdZTQT3CYvcZXRHR8WdzeKh1hQ5aqZVm5FY1W0AU'
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image
+        imgUrl = {this.state.imgUrl}
+        />
       </div>
     );
   }
---
+++ app-5/src/Image.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default (props) => {
+    return (
+        <img src={props.imgUrl} alt="Lucina"/>
+    )
+}
