45061de9688a8027922340f9b26350324eca4a8e
McKay Waters <mckaypaswaters@gmail.com>
2019-09-19 22:50:42 -0600

    finished drill 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Todo from './Todo'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todoArr: [],
+      input: ''
+    }
+    this.add = this.add.bind(this)
+  }
+
+  handleChange(e){
+    this.setState({input: e})
+  }
+
+  add(){
+    this.setState({
+      todoArr: [...this.state.todoArr, this.state.input],
+      input: ''
+    })
+  }
+
   render() {
+    let mappedArr = this.state.todoArr.map((element, index) => {
+      return  <p>{element}</p>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <input value={this.state.input} onChange={(e) => this.handleChange(e.target.value)}type="text"/>
+        <button onClick={this.add}>Add</button>
+        <Todo
+        todo={mappedArr}
+        />
       </div>
     );
   }
